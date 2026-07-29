a3ed65f3e371f62f279112aa107db0e5d25698db
JULI <admi@MacBook-Air-de-admi.local>
2026-07-28 21:10:49 -0500

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/prueba-viralco/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/Prueba-viralco/' : '/prueba-viralco/',
   plugins: [react()],
   define: {
     global: 'globalThis',
